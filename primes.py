67fd096e707d15d7ed7fb76c3be8283f70a83604
Bivas Aryal <bivas.aryal@kcl.ac.uk>
2023-09-23 22:39:16 +0100

    Add code to list primes

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+    primes = 0 
+    next = 2
+    while primes < number_of_primes:
+        if is_prime(next):
+            list.append(next)
+            primes += 1
+        next += 1
     return list
+
+
+
+
+def is_prime(val):
+
+    if val%2==0:
+        return False
+
+    for i in range(3,int((val-1)/2)):
+        if val%i == 0: 
+            return False
+        
+    return True 
